46b5070f4a71f1574c369433ce888d8966e7ae49
Danilo Ribeiro <danriba@gmail.com>
2021-05-03 21:27:20 -0300

    Creating Wit_Connector class

--- src/model/wit_connector.py
+++ src/model/wit_connector.py
@@ -1,13 +1,15 @@
 from wit import Wit
+import pprint as pp
+import os
 
 class Wit_Connector:
-  def __init__(self):
-    self.client = Wit("$WIT_TOKEN")
-    print("$WIT_TOKEN")
+    def __init__(self):
+        self.client = Wit(os.environ['WIT_TOKEN'])
 
-  def send_test_message(self):
-    self.client.message('Olá! Gostaria de uma pizza!')
-    return self.client
-
-  
-client = Wit_Connector()
+    def send_test_message(self):
+        resp = self.client.message("test message")
+        pp.pprint('Yay, got Wit.ai response: ' + str(resp))
+    
+    def message(self, message):
+        resp = self.client.message(message)
+        return resp
