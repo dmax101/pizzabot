94068d7c5d06ffcec6241ce13c6aa5c73fea74fc
luanagribel <luanagribel@hotmail.com>
2021-06-28 03:53:04 +0000

    db queries config

--- src/controller/dbcontroller.py
+++ src/controller/dbcontroller.py
@@ -6,19 +6,44 @@ class DbController:
     def __init__(self, collection):
         self.collection = collection[0]
 
+    #novo pedido
     def post_one_doc(self, document):
         self.collection.insert_one(document)
     
+    #revisao pedido
     def get_one_doc(self, document):
         res = self.collection.find_one(document)
         return res
 
-# cnn = DbConnector("pizzabot", os.environ["DB_PASSWORD"], "pizzabot")
+    #cardapio
+    def find_all(self):
+      res = self.collection.find({})
+      return res
+
+cnn = DbConnector("pizzabot", os.environ["DB_PASSWORD"], "pizzabot")
+
+##### PEDIDO #####
 
 # order_db = cnn.connect().orders
 
 # order_handler = DbController([order_db])
 
+# novo
+
 # order_handler.post_one_doc({"teste": "teste"})
 
+# revisao
+
 # pp.pprint(order_handler.get_one_doc({"teste": "teste"}))
+
+
+##### CARDAPIO #####
+
+#pizzas_db = cnn.connect().pizzas
+
+#pizzas_handler = DbController([pizzas_db])
+
+#pizzas = pizzas_handler.find_all()
+
+#for pizza in pizzas:
+# print(pizza)
